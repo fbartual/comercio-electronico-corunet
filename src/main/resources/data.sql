e7553536b0aebcffea27a7443a94a9877ade00cb
fbartual <f.bartual@udc.es>
2021-12-27 17:30:24 +0100

    Subida de tabla Brand restante

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
+INSERT INTO Brand (name)
+	VALUES ('ZARA');
+
 INSERT INTO Prices (brandId, startDate, endDate, priceList, productId, priority, price, curr)
 	VALUES (1, '2020-06-14 00.00.00', '2020-12-31 23.59.59', 1, 35455, 0, 35.50, 'EUR'),
 	       (1, '2020-06-14 15.00.00', '2020-06-14 18.30.00', 2, 35455, 1, 25.45, 'EUR'),
